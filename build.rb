5907edb8fada9df9228043f030b434601e7dcb74
Kenneth Lim <kenlimmj@gmail.com>
2013-10-20 17:33:15 -0400

    Oracle built on 2013-10-20 17:33:15 -0400

--- build.rb
+++ build.rb
@@ -41,7 +41,8 @@ class BlogBuilder
     def set_post_metadata
       @posts = []
       Dir.foreach('./content/posts/') do |item|
-        next if item == '.' or item == '..' or item =='.DS_Store'
+        # next if item == '.' or item == '..' or item =='.DS_Store'
+        next if item != '*.md'
         title = File.open("./content/posts/#{item}", &:readline).unpack("C*").pack("U*")
         title = title.gsub("\n", "")
         title[0] = ''
